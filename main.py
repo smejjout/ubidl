e7703e74e3188fd154a73f4dfcae534164a4f6b2
smejjout <sofian.mejjoute@student.unamur.be>
2020-11-27 14:45:14 +0100

    implemented ffmpeg-python

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Downloader for ubicast media servers
 
-import os
 import sys
 import re
 import requests
 import json
+import ffmpeg
 
 with open("config.json") as config_file:
     config = json.load(config_file)
@@ -18,21 +18,26 @@ except IndexError:
         {sys.argv[0]} <permalink>")
     exit()
 
-oid = re.sub('.*permalink',"", url).strip("/")
+oid = re.sub('.*permalink', "", url).strip("/")
 params = {
     "api_key": api_key,
     "oid": oid,
     "html5": "mp4_mp3_m3u8"
 }
 
-res = requests.get(f"{ubicast_server}/api/v2/medias/modes/", params=params, verify=False)
+res = requests.get(f"{ubicast_server}/api/v2/medias/modes/",
+                   params=params, verify=False)
 dict_vid = json.loads(res.content)
 url_vid = dict_vid["720p"]["resource"]["url"]
 url_audio = dict_vid["audio"]["tracks"][0]["url"]
 
 params.pop("html5")
-res = requests.get(f"{ubicast_server}/api/v2/medias/get/", params=params, verify=False)
+res = requests.get(f"{ubicast_server}/api/v2/medias/get/",
+                   params=params, verify=False)
 info_vid = json.loads(res.content)
 title = info_vid["info"]["title"]
 
-os.system(f"ffmpeg -i '{url_vid}' -i '{url_audio}' -bsf:a aac_adtstoasc -c:v copy -c:a copy {title}.mkv")
+audio = ffmpeg.input(url_audio)
+video = ffmpeg.input(url_vid)
+stream = ffmpeg.output(audio, video, title+".mkv", codec="copy")
+ffmpeg.run(stream)
