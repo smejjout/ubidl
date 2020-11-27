bd4ed3647fd289773238f5122858763a1390bb6f
smejjout <sofian.mejjoute@student.unamur.be>
2020-11-27 15:21:07 +0100

    implemented multiple urls support

--- main.py
+++ main.py
@@ -6,38 +6,45 @@ import requests
 import json
 import ffmpeg
 
+
+if len(sys.argv) == 1:
+    print(f"Usage:\
+        {sys.argv[0]} <permalink>")
+    exit()
+
 with open("config.json") as config_file:
     config = json.load(config_file)
     api_key = config["api_key"]
     ubicast_server = config["ubicast_server"]
 
-try:
-    url = sys.argv[1]
-except IndexError:
-    print(f"Usage:\
-        {sys.argv[0]} <permalink>")
-    exit()
+urls = sys.argv[1:]
 
-oid = re.sub('.*permalink', "", url).strip("/")
-params = {
-    "api_key": api_key,
-    "oid": oid,
-    "html5": "mp4_mp3_m3u8"
-}
 
-res = requests.get(f"{ubicast_server}/api/v2/medias/modes/",
-                   params=params, verify=False)
-dict_vid = json.loads(res.content)
-url_vid = dict_vid["720p"]["resource"]["url"]
-url_audio = dict_vid["audio"]["tracks"][0]["url"]
+def download(url):
+    oid = re.sub('.*permalink', "", url).strip("/")
+    params = {
+        "api_key": api_key,
+        "oid": oid,
+        "html5": "mp4_mp3_m3u8"
+    }
 
-params.pop("html5")
-res = requests.get(f"{ubicast_server}/api/v2/medias/get/",
-                   params=params, verify=False)
-info_vid = json.loads(res.content)
-title = info_vid["info"]["title"]
+    res = requests.get(f"{ubicast_server}/api/v2/medias/modes/",
+                       params=params, verify=False)
+    dict_vid = json.loads(res.content)
+    url_vid = dict_vid["720p"]["resource"]["url"]
+    url_audio = dict_vid["audio"]["tracks"][0]["url"]
 
-audio = ffmpeg.input(url_audio)
-video = ffmpeg.input(url_vid)
-stream = ffmpeg.output(audio, video, title+".mkv", codec="copy")
-ffmpeg.run(stream)
+    params.pop("html5")
+    res = requests.get(f"{ubicast_server}/api/v2/medias/get/",
+                       params=params, verify=False)
+    info_vid = json.loads(res.content)
+    title = info_vid["info"]["title"]
+
+    audio = ffmpeg.input(url_audio)
+    video = ffmpeg.input(url_vid)
+    stream = ffmpeg.output(audio, video, title+".mkv", codec="copy")
+    ffmpeg.run(stream)
+
+
+for url in urls:
+    download(url)
